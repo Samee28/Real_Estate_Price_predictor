4de5a21a0fd0d5e9c1f09424593e33fe070456d3
sameeksha rai <158418707+Samee28@users.noreply.github.com>
2025-05-05 10:40:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 model = pickle.load(open('home_price_model.pkl', 'rb'))  # Make sure model.pkl is in same folder
@@ -26,5 +27,11 @@ def predict():
     except Exception as e:
         return render_template('index.html', prediction_text=f"Error: {str(e)}")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
+
